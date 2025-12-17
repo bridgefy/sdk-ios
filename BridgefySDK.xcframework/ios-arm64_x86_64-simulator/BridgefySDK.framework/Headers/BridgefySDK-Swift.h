808ee7d954e4c78e69b717a71b25f82f80822552
Francisco <francisco@bridgefy.me>
2025-12-16 18:59:01 -0600

    chore: remove xcframework from git and ignore binaries

--- BridgefySDK.xcframework/ios-arm64_x86_64-simulator/BridgefySDK.framework/Headers/BridgefySDK-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:461399c6b72f8a9e670d4648195e1eb3f74cc98ec0d00f1d806a780f9d545f41
-size 20517
--- BridgefySDK.xcframework/ios-arm64_x86_64-simulator/BridgefySDK.framework/Headers/BridgefySDK.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7e5f1d59d6e7d17ac62cf47d93a905e9cd5a621f7671ae6d1228d7d6f1faba22
-size 446
