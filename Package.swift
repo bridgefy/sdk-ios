6da963fe6a7e264f71a38f66c7dcb14aaf52fb4f
Francisco <francisco@bridgefy.me>
2026-05-14 09:35:59 -0600

    chore: Update xcframework

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BridgefySDK",
-            url: "https://github.com/bridgefy/sdk-ios/releases/download/1.3.4/BridgefySDK.xcframework.zip",
-            checksum: "b89060551556ac7e4bd9c953b81df1a59939696fa8c0e53de19ea48e6a343db5"
+            url: "https://github.com/FranciscoMkdir/SDK-iOS-binary/releases/download/1.0.0/BridgefySDK.xcframework.zip",
+            checksum: "bdc47013ecb147ae8b8b206f28efab7aa43e64c0fe4377159adf1b63faed6323"
         )
     ]
 )
